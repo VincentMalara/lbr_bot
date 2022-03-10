f7fdeeffa97394c7ccaac75255d3c4c62e30b3f1
VMNEOS <you@example.com>
2022-03-10 14:18:16 +0100

    merge old publi 2

--- src/Merge_old_publi_2.py
+++ src/Merge_old_publi_2.py
@@ -2,14 +2,13 @@ from configs import settings
 from src.pdf_downloaders.utils import PdfDownloader, check_temp_exist
 from src.mongo.main import mongo
 
-check_temp_exist() #needed in case temp folder is not in the project
+import pandas as pd
 
-mongo_DB = settings.mongo_DB
-col_RCSp = settings.col_RCSp
-col_pdfs = settings.col_pdfs
+check_temp_exist() #needed in case temp folder is not in the project
 
 
 Mongo_publi_old = mongo(ip='146.59.152.231', db='LBR_test', col='publi_old')
+#Mongo_publi_old = mongo(db='LBR_new', col='LBR_Adm_and_Asso')
 
 Mongorcsp = mongo(ip='146.59.152.231', db='LBR_test', col='RCS_parsed')
 
@@ -19,7 +18,28 @@ print(f"building pdf list")
 pdfdownloader = PdfDownloader( mongo_rcsparsed=Mongorcsp, mongo_pdfs=Mongo_allpdfs)
 pdf_list = pdfdownloader.get_pdfs_list()
 print(pdf_list)
+print(' - - ')
 
 
-Mongo_publi_old.find(dictin={'N_depot': {'$in': pdf_list['depot'].unique().tolist() }},
+DF = Mongo_publi_old.find(dictin={'N_depot': {'$in': pdf_list['N_depot'].unique().tolist() }},
                      dictout={'N_depot': 1,'file':1,'extraction_date':1 , '_id': 0})
+
+print(DF.shape)
+if DF.shape[0]>0:
+    DF['newdate'] = pd.to_datetime(DF['extraction_date'], format='%d/%m/%Y')
+    DF.sort_values(by='newdate', ascending=False, inplace=True)
+    DF.drop_duplicates(subset='N_depot',keep='first', inplace=True)
+    print(DF.shape)
+    DF.drop(columns=['newdate'], inplace=True)
+
+
+    DF2 = pdf_list[pdf_list['N_depot'].isin(DF['N_depot'].to_list())].reset_index(drop=True)
+
+    DF = DF.merge(DF2, on ='N_depot', how='left')
+
+    DF = DF[DF['file'].notna()].reset_index(drop=True)
+
+
+    Mongo_allpdfs.insert(DF, col='N_depot')
+
+
