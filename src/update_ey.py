aca2b4ec14574ac4275beecc6ee6fff2e0e74af2
VMNEOS <you@example.com>
2022-03-11 07:42:20 +0100

    new update ey

--- src/update_ey.py
+++ src/update_ey.py
@@ -1,9 +1,9 @@
 from configs import settings
 from src.scrapers.main import main as scraper
 from src.html_parsers.main import main as rcs_parser
-#from src.pdf_downloaders.main import main as pdf_downloader
-#from src.pdf_parsers.financials.main import main as financials_parser
-#from src.pdf_parsers.publications.main import main as publi_parser
+from src.pdf_downloaders.main import main as pdf_downloader
+from src.pdf_parsers.financials.main import main as financials_parser
+from src.pdf_parsers.publications.main import main as publi_parser
 
 from .utils.timer import performance_timer
 from src.mongo.main import mongo
@@ -18,12 +18,15 @@ Mongorbe = mongo(ip='146.59.152.231', db='LBR_test', col='RBE')
 Mongorcsp = mongo(ip='146.59.152.231', db='LBR_test', col='RCS_parsed')
 Mongorbep = mongo(ip='146.59.152.231', db='LBR_test', col='RBE_parsed')
 Mongoresa = mongo(ip='146.59.152.231', db='LBR_test', col='RESA_parsed')
+Mongopdf= mongo(ip='146.59.152.231', db='LBR_test', col='all_pdfs')
+Mongopubli = mongo(ip='146.59.152.231', db='LBR_test', col='publications')
+Mongofinan = mongo(ip='146.59.152.231', db='LBR_test', col='financials')
 
 
 #Mongorcs.set_status(newstatus='scraped',dictin={'status' : 'to_be_updated', 'extraction_date':{'$nin':["10/03/2022"]}})
 #Mongorbe.set_status(newstatus='scraped',dictin={'status' : 'to_be_updated', 'extraction_date':{'$nin':["10/03/2022"]}})
 
-
+'''
 DF = Mongorcsp.find({'Forme juridique':{ '$regex' : '.*' + 'commandite spéciale' + '.*'}})
 RCS_SCSP_exist = DF['RCS'].to_list()
 print(len(RCS_SCSP_exist))
@@ -65,7 +68,7 @@ print(len(RCSlist))
 
 #Mongorcs.set_to_be_updated(RCS=RCSlist)
 #Mongorbe.set_to_be_updated(RCS=RCSlist)
-
+'''
 
 #Mongorcs.set_to_be_updated(RCS=DF2["ns2:NumeroRCS"].to_list(), dictin={'extraction_date':{'$nin':['10/03/2022']}})
 #Mongorcs.insert_empty_RCS(RCSlist,  update_existing=False)
@@ -77,17 +80,36 @@ print('----RCS Scraped---')
 
 # 3 - parse RCS of RCS list
 print('----Parsing RCS---')
-rcs_parser(type_='rcs', mongo=Mongorcs, mongoparsed=Mongorcsp,  onlynew=False)
+rcs_parser(type_='rcs', mongo=Mongorcs, mongoparsed=Mongorcsp,  onlynew=True)
 print('----RCS Parsed---')
 
-'''
+
 # 4 - scrap RCS list in RBE: only the one in no ro new as changed RCS
 print('----Scraping RBE---')
 RBElist = scraper(type_='RBE', mongo=Mongorbe, to_be_updated=True)
 print('----RBE Scraped---')
 
 # 5 - parse RBE of RCS list
-print('----Parsing RCS---')
+print('----Parsing RBE---')
 rcs_parser(type_='rbe', mongo=Mongorbe, mongoparsed=Mongorbep,  onlynew=True)
-print('----RCS Parsed---')
+print('----RCBE Parsed---')
+
+RCSlist = Mongorcsp.get_RCSlist(dictin={"Forme juridique":{ '$regex' : '.*' + 'commandite spéciale' + '.*'}})
+print(len(RCSlist))
+
+# 6 - download new pdf not yet downloaded
+print('----Downloading pdfs---')
+pdf_downloader(RCS=RCSlist,mongo_rcsparsed=Mongorcsp, mongo_pdfs=Mongopdf) #always consider only new "depot"
+print('----pdfs Downloaded ---')
+
 '''
+# 7 - parsing bilan from downloaded pdf
+print('----parsing bilans---')
+financials_parser(RCS=RCSlist,mongo=Mongopdf, mongoparsed=Mongofinan, onlynew=False)
+print('----bilans parsed ---')
+'''
+
+# 8 - parsing publi from downloaded pdf
+print('----parsing publi---')
+publi_parser(RCS=RCSlist,mongo=Mongopdf, mongoparsed=Mongopubli, onlynew=False)
+print('----publi parsed ---')
